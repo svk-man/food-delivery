8f1a0520e77484d0628a68d4bb31232c10010263
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-08-11 15:34:26 +0500

    feat: add birth date input

---
+++ src/features/registration-form/lib/isValidDate.ts
@@ -0,0 +1,12 @@
+export function isValidDate(date: string): boolean | string {
+    if (!date.length) return 'Обязательное поле';
+
+    const dateObject = new Date(date);
+    const currentDate = new Date();
+    if (!dateObject.getDate() || dateObject.getTime() > Date.now()) return 'Некорректная дата';
+
+    const ageDifference = currentDate.getFullYear() - dateObject.getFullYear();
+    if (ageDifference < 13) return 'Минимальный возраст для регистрации 13 лет';
+
+    return true;
+}
