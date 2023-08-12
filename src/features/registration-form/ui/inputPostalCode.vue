2052fce027ef5736cb679b2c92698e86731f1306
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-08-12 15:52:44 +0500

    feat: add input postal code component

---
+++ src/features/registration-form/ui/inputPostalCode.vue
@@ -0,0 +1,25 @@
+<template>
+    <q-input
+        label="Индекс"
+        outlined
+        clearable
+        stack-label
+        color="accent"
+        reactive-rules
+        v-model="userPostalCode"
+        class="signup-form__input text-body1"
+    />
+    <!-- @update:model-value="updateEmail" -->
+</template>
+
+<script setup lang="ts">
+import { defineComponent, ref } from 'vue';
+
+const userPostalCode = ref('');
+
+defineComponent({
+    name: 'InputPostalCode',
+});
+</script>
+
+<style scoped></style>
