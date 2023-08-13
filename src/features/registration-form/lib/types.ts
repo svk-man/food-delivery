8e675d36d5c9b7dfcffd3a435146611d825b7656
Stepan Shtennikov <shtennikov.s@gmail.com>
2023-08-13 11:20:03 +0500

    feat: add password input

--- src/features/registration-form/lib/types.ts
+++ src/features/registration-form/lib/types.ts
@@ -2,3 +2,11 @@ export interface Country {
     name: string;
     postalCodePattern: string;
 }
+
+export interface PasswordValidator {
+    isValidLength: boolean;
+    isValidCapital: boolean;
+    isValidLowercase: boolean;
+    isValidDigit: boolean;
+    isValid: boolean;
+}
