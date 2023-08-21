ef296537807421674b621a9667f3780935a46fbb
Svetlana Trukhina <sweta4ca@mail.ru>
2023-08-21 19:26:53 +0700

    feat: add separate shipping, billing, default values

--- src/features/registration-form/ui/inputPostalCode.vue
+++ src/features/registration-form/ui/inputPostalCode.vue
@@ -7,6 +7,7 @@
         color="accent"
         reactive-rules
         v-model="userPostalCode"
+        @update:model-value="updatePostal"
         class="signup-form__input postal-code-input text-body1"
     />
 </template>
@@ -16,6 +17,11 @@ import { defineComponent, ref } from 'vue';
 
 const userPostalCode = ref('');
 
+const emit = defineEmits(['update:postal']);
+
+const updatePostal = (value: string | null): void => {
+    if (value && typeof value === 'string') emit('update:postal', value);
+};
 defineComponent({
     name: 'InputPostalCode',
 });
