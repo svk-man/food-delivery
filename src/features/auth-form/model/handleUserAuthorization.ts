bacedca3ed12b066eac74c298e70be6b3493af23
Svetlana Trukhina <sweta4ca@mail.ru>
2023-08-25 00:08:13 +0700

    feat: add login functional

--- src/features/auth-form/model/handleUserAuthorization.ts
+++ src/features/auth-form/model/handleUserAuthorization.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
 import { Notify } from 'quasar';
-import { manageToken } from 'src/shared/api/auth';
 import { Customer } from '../../registration-form/lib/types';
 
 export interface MyCustomerSignin {
@@ -11,20 +10,63 @@ export interface CustomerSignInResult {
     customer: Customer;
 }
 
+export interface PasswordFlowReturned {
+    access_token: string;
+    expires_in: string;
+    refresh_token: string;
+    scope: string;
+    token_type: string;
+}
+
+export async function fetchTokenWithPasswordFlow(
+    password: string,
+    email: string
+): Promise<PasswordFlowReturned | null> {
+    const id: string = import.meta.env.VITE_SPA_CLIENT_ID;
+    const secret: string = import.meta.env.VITE_SPA_CLIENT_SECRET;
+    const scope: string = import.meta.env.VITE_SPA_SCOPE;
+
+    try {
+        const response: AxiosResponse<PasswordFlowReturned> = await axios({
+            url: 'https://auth.us-central1.gcp.commercetools.com/oauth/carrot78/customers/token',
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: `Basic ${btoa(`${id}:${secret}`)}`,
+            },
+            params: {
+                grant_type: 'password',
+                username: email,
+                password,
+                scope,
+            },
+        });
+        Notify.create('Успешный вход! meow~ 🐈🐈');
+
+        return response.data;
+    } catch (error) {
+        return null;
+    }
+}
+
 export default async function handleUserAuthorization(password: string, email: string): Promise<void | null> {
     const data: MyCustomerSignin = { email, password };
-    const token = await manageToken();
+
+    const tokenWithExpireTime = await fetchTokenWithPasswordFlow(password, email);
+    if (!tokenWithExpireTime) return null;
+
     try {
-        const response: AxiosResponse<{ results: CustomerSignInResult | undefined }> = await axios({
+        const response: AxiosResponse<undefined | null> = await axios({
             url: 'https://api.us-central1.gcp.commercetools.com/carrot78/me/login',
             method: 'post',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
+                Authorization: `Bearer ${tokenWithExpireTime.access_token}`,
             },
             data: JSON.stringify(data),
         });
         Notify.create('Успешный вход! meow~ 🐈🐈');
+
         return Promise.resolve();
     } catch (error) {
         return null;
