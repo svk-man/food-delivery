d794420691377f2aa3e18dc9f558d4f04868b93d
Svetlana Trukhina <sweta4ca@mail.ru>
2023-08-24 17:41:06 +0700

    feat: add login with commerse tools

--- src/features/auth-form/model/handleUserAuthorization.ts
+++ src/features/auth-form/model/handleUserAuthorization.ts
@@ -1,3 +1,32 @@
-export default function handleUserAuthorization(password: string, email: string): void {
-    // logic of authorization
+import axios, { AxiosResponse } from 'axios';
+import { Notify } from 'quasar';
+import { manageToken } from 'src/shared/api/auth';
+import { Customer } from '../../registration-form/lib/types';
+
+export interface MyCustomerSignin {
+    email: string;
+    password: string;
+}
+export interface CustomerSignInResult {
+    customer: Customer;
+}
+
+export default async function handleUserAuthorization(password: string, email: string): Promise<void | null> {
+    const data: MyCustomerSignin = { email, password };
+    const token = await manageToken();
+    try {
+        const response: AxiosResponse<{ results: CustomerSignInResult | undefined }> = await axios({
+            url: 'https://api.us-central1.gcp.commercetools.com/carrot78/me/login',
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            data: JSON.stringify(data),
+        });
+        Notify.create('Успешный вход! meow~ 🐈🐈');
+        return Promise.resolve();
+    } catch (error) {
+        return null;
+    }
 }
