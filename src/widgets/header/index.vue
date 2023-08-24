e609128f1da96dcf04864f925cb7157d1d1844fd
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-08-24 12:50:20 +0300

    feat: add text logo

--- src/widgets/header/index.vue
+++ src/widgets/header/index.vue
@@ -1,59 +1,48 @@
 <template>
-    <q-header class="bg-secondary q-pa-sm">
-        <q-toolbar class="header q-py-sm bg-primary">
-            <div class="header__content">
-                <q-toolbar-title>
-                    <q-img src="/logo.png" :style="{ width: '80px' }" />
-                </q-toolbar-title>
-                <div class="header__buttons font-inter-500">
-                    <div class="gt-sm">
-                        <q-btn to="/" label="Главная" class="header__buttons-item" rounded text-color="black" flat />
-                        <q-btn
-                            to="/catalog"
-                            label="Каталог"
-                            class="header__buttons-item"
-                            rounded
-                            text-color="black"
-                            flat
-                        />
-                        <q-btn
-                            to="/cart"
-                            label="Корзина"
-                            class="header__buttons-item"
-                            rounded
-                            text-color="black"
-                            flat
-                        />
-                    </div>
-                    <div class="header__buttons_auth gt-sm">
-                        <q-btn
-                            to="/register"
-                            label="Регистрация"
-                            icon="add"
-                            class="header__buttons-item header__buttons-item_auth"
-                            color="orange-5"
-                            text-color="white"
-                        />
-                        <q-btn
-                            to="/signin"
-                            label="Войти"
-                            icon-right="key"
-                            class="header__buttons-item header__buttons-item_auth"
-                            color="orange-5"
-                            text-color="white"
-                        />
-                    </div>
-                    <div class="lt-md">
-                        <q-btn
-                            text-color="black"
-                            flat
-                            dense
-                            round
-                            icon="menu"
-                            aria-label="Menu"
-                            @click="toggleLeftDrawer"
-                        />
-                    </div>
+    <q-header class="header bg-secondary q-pa-sm">
+        <q-toolbar class="header__toolbar q-py-md bg-primary">
+            <q-toolbar-title>
+                <a class="logo" href="/">
+                    <h1 class="logo__text font-inter-900">
+                        <span>Food</span>
+                        Delivery
+                    </h1>
+                </a>
+            </q-toolbar-title>
+            <div class="header__buttons font-inter-500">
+                <div class="gt-sm">
+                    <q-btn to="/" label="Главная" class="header__buttons-item" rounded text-color="black" flat />
+                    <q-btn to="/catalog" label="Каталог" class="header__buttons-item" rounded text-color="black" flat />
+                    <q-btn to="/cart" label="Корзина" class="header__buttons-item" rounded text-color="black" flat />
+                </div>
+                <div class="header__buttons_auth gt-sm">
+                    <q-btn
+                        to="/register"
+                        label="Регистрация"
+                        icon="add"
+                        class="header__buttons-item header__buttons-item_auth"
+                        color="orange-5"
+                        text-color="white"
+                    />
+                    <q-btn
+                        to="/signin"
+                        label="Войти"
+                        icon-right="key"
+                        class="header__buttons-item header__buttons-item_auth"
+                        color="orange-5"
+                        text-color="white"
+                    />
+                </div>
+                <div class="lt-md">
+                    <q-btn
+                        text-color="black"
+                        flat
+                        dense
+                        round
+                        icon="menu"
+                        aria-label="Menu"
+                        @click="toggleLeftDrawer"
+                    />
                 </div>
             </div>
         </q-toolbar>
@@ -94,13 +83,9 @@ defineComponent({
 <style lang="scss" scoped>
 .header {
     max-width: 1576px;
-    margin: 0 auto;
-    border-radius: 12px;
-    display: flex;
 
-    &__content {
-        display: flex;
-        flex-basis: 100%;
+    &__toolbar {
+        border-radius: 12px;
     }
 
     &__buttons {
@@ -121,6 +106,26 @@ defineComponent({
             gap: 12px;
             height: fit-content;
         }
+    }
+}
+
+.logo {
+    display: inline-block;
+    width: 230px;
+    text-decoration: none;
+    color: $deep-purple-10;
+    transition: 0.3s;
+    &__text {
+        margin: 0;
+        font-size: 30px;
+        line-height: 35px;
+        text-transform: uppercase;
+        span {
+            color: $orange-5;
+        }
+    }
+    &:hover {
+        color: $orange-5;
     }
 }
 
