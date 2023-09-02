bcd3e8bc5fef34c84c6d3ff9ab672ad564ba18c2
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-09-03 01:01:03 +0300

    fix: show trunced description for product card

--- src/entities/product/ui/index.vue
+++ src/entities/product/ui/index.vue
@@ -4,19 +4,32 @@
 
         <q-card-section>
             <h3 class="text-h5 q-mt-sm q-mb-xs">{{ title }}</h3>
-            <p class="text-caption text-grey">{{ description }}</p>
+            <p class="text-caption text-grey">{{ truncedDescription }}</p>
         </q-card-section>
     </q-card>
 </template>
 
 <script setup lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
 import styles from './styles.module.scss';
 
-defineProps({
+const props = defineProps({
     title: { type: String, required: true },
     description: { type: String, required: true },
     imageSrc: { type: String, required: true },
+});
+
+const truncedDescription = computed(() => {
+    const DESCRIPTION_MAX_LENGTH = 30;
+    const DESCRIPTION_MAX_WORDS_COUNT = 34;
+
+    if (props.description.length <= DESCRIPTION_MAX_LENGTH) {
+        return props.description;
+    }
+
+    const splittedDescription = props.description.split(' ');
+
+    return `${splittedDescription.slice(0, DESCRIPTION_MAX_WORDS_COUNT).join(' ')}...`;
 });
 
 defineComponent({
